65f1dc6e28f7757ca67f7683bd0b8abdcacc7f43
Jordi Blasco Pallares <jordi.blasco@gmail.com>
2018-02-10 09:12:30 +1300

    Update postinstall.sh

--- debian/postinstall.sh
+++ debian/postinstall.sh
@@ -16,16 +16,10 @@
 ### You should have received a copy of the GNU General Public License
 ### along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### 
-version=release-1.1.13
 cd /root
-git clone http://bitbucket.org/hpcnow/snow-tools.git -b $version
-cd snow-tools
 wget -O snow.conf "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/snow.conf" --no-check-certificate
 if [[ -e /sNow/snow-tools/etc/snow.conf ]]; then
     wget -O /etc/network/interfaces "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/interfaces_snow02" --no-check-certificate
 else
     wget -O /etc/network/interfaces "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/interfaces_snow01" --no-check-certificate
 fi
-export SNOW_EULA=accepted
-./install.sh $version
-
