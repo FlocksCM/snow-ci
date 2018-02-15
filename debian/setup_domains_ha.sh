af0a91017868371c8bafbd64116f5ac2be8fc8aa
Jordi Blasco <jordi.blasco@hpcnow.com>
2018-02-15 05:14:04 +0100

    extended setup_domains_ha.sh

--- debian/setup_domains_ha.sh
+++ debian/setup_domains_ha.sh
@@ -5,6 +5,7 @@ rm -f pacemaker.cfg
 echo "property stonith-enabled=no" > pacemaker.cfg
 echo "property no-quorum-policy=ignore" >>  pacemaker.cfg
 echo "property default-resource-stickiness=100" >> pacemaker.cfg
+echo "primitive xsnow-vip ocf:heartbeat:IPaddr2 params ip=\"10.1.0.254\" nic=\"xsnow0\" op monitor interval=\"10s\"" >> pacemaker.cfg
 for domain in ${domain_list} do
     echo "primitive $domain ocf:heartbeat:Xen \\
           params xmfile=\"/sNow/snow-tools/etc/domains/$domain.cfg\" \\
