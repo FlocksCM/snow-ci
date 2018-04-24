b08e09876b3830515fba71929b7ec45cf06978ac
Jordi Blasco Pallares <jordi.blasco@gmail.com>
2018-04-25 07:33:33 +1200

    Included BeeGFS client mount binds

--- debian/postinstall.sh
+++ debian/postinstall.sh
@@ -16,6 +16,9 @@
 ### You should have received a copy of the GNU General Public License
 ### along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### 
+
+
+### sNow Servers Configuration
 cd /root/snow-tools
 wget -O snow.conf "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/snow.conf" --no-check-certificate
 if [[ -e /sNow/snow-tools/etc/snow.conf ]]; then
@@ -27,8 +30,21 @@ fi
 cd /root
 wget -O hosts "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/hosts" --no-check-certificate
 cat ./hosts >> /etc/hosts
+
+### HA Configuration
 wget -O corosync.conf "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/corosync.conf" --no-check-certificate
 wget -O active-domains.conf "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/active-domains.conf" --no-check-certificate
 wget -O setup_domains_ha.sh "https://raw.githubusercontent.com/HPCNow/snow-ci/master/debian/setup_domains_ha.sh" --no-check-certificate
 chmod 700 setup_domains_ha.sh
 
+
+### BeeGFS Configuration
+cd /etc/apt/sources.list.d/
+wget https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
+wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | apt-key add -
+apt update
+apt upgrade -y
+echo "
+/beegfs/snow/testing/home                     /home          none  noauto,x-systemd.automount,x-systemd.device-timeout=60,_netdev,bind,x-systemd.requires=/beegfs/snow/testing/home   0 0
+/beegfs/snow/testing/sNow                     /sNow          none  noauto,x-systemd.automount,x-systemd.device-timeout=60,_netdev,bind,x-systemd.requires=/beegfs/snow/testing/sNow 0 0
+" >> /etc/fstab
